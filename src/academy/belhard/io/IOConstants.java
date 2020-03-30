d4ec4ab5effa37f1e774c3b0b1678052fc75eb0e
ekhrapan <yauheni.khrapan@gmail.com>
2020-03-30 19:13:48 +0300

    filename fixed

--- src/academy/belhard/io/IOConstants.java
+++ src/academy/belhard/io/IOConstants.java
@@ -2,6 +2,6 @@ package academy.belhard.io;
 
 public class IOConstants {
 
-    public static final String FILENAME = "data.csv";
+    public static final String FILENAME = "D:\\garbage\\data.csv";
 
 }
